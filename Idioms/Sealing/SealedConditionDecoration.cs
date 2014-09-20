f3c0d5829771eadc50c71cfddd15e250bb662fe5
Piirtaa <piirtaa@gmail.com>
2014-09-20 19:15:30 -0400

    cont'd

---
+++ Idioms/Sealing/SealedConditionDecoration.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using CuttingEdge.Conditions;
+using Decoratid.Idioms.Core.Conditional;
+using Decoratid.Idioms.ObjectGraph;
+using Decoratid.Idioms.ObjectGraph.Values;
+using Decoratid.Idioms.Core.Logical;
+using Decoratid.Idioms.Core;
+
+namespace Decoratid.Idioms.Validating
+{
+    public interface IHasValidator
+    {
+        ICondition IsValidCondition {get;}
+    }
+
+    public class ValidatingConditionDecoration : DecoratedConditionBase, IHasValidator
+    {
+        #region Ctor
+        public ValidatingConditionDecoration(ICondition decorated, ICondition isValidCondition)
+            : base(decorated)
+        {
+            Condition.Requires(isValidCondition).IsNotNull();
+        }
+        #endregion
+
+        #region Properties
+        public ICondition IsValidCondition {get; private set;}
+        #endregion
+
+        #region Methods
+        public override bool? Evaluate()
+        {
+            if(this.IsValidCondition
+                return base.Evaluate();
+        }
+        public override IDecorationOf<ICondition> ApplyThisDecorationTo(ICondition thing)
+        {
+            return new LoggingConditionDecoration(thing);
+        }
+        #endregion
+    }
+
+
+}
---
+++ Idioms/Sealing/SealedLogicDecoration.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using CuttingEdge.Conditions;
+using Decoratid.Idioms.Core.Conditional;
+using Decoratid.Idioms.ObjectGraph;
+using Decoratid.Idioms.ObjectGraph.Values;
+using Decoratid.Idioms.Core.Logical;
+using Decoratid.Idioms.Core;
+
+namespace Decoratid.Idioms.Validating
+{
+
+    public class ValidatingLogicDecoration : DecoratedLogicBase
+    {
+        #region Ctor
+        public LoggingLogicDecoration(ILogic decorated, , ICondition isValidCondition)
+            : base(decorated)
+        {
+            Logger = logger;
+        }
+        #endregion
+
+        #region Properties
+        public ICondition IsValidCondition {get; private set;}
+        #endregion
+
+        #region Methods
+        public override void Perform()
+        {
+            if (this.Logger != null)
+                this.Logger.LogVerbose("Perform started", null);
+
+            try
+            {
+                Decorated.Perform();
+            }
+            catch (Exception ex)
+            {
+                if (this.Logger != null)
+                    this.Logger.LogError(ex.Message, null, ex);
+
+                throw;
+            }
+            finally
+            {
+                if (this.Logger != null)
+                    this.Logger.LogVerbose("Perform completed", null);
+            }
+        }
+        public override IDecorationOf<ILogic> ApplyThisDecorationTo(ILogic thing)
+        {
+            if (this.Logger != null)
+                this.Logger.LogVerbose("ApplyThisDecorationTo started", thing);
+
+            IDecorationOf<ILogic> rv = null;
+            try
+            {
+                rv = new LoggingLogicDecoration(thing);
+            }
+            catch (Exception ex)
+            {
+                if (this.Logger != null)
+                    this.Logger.LogError(ex.Message, null, ex);
+
+                throw;
+            }
+            finally
+            {
+                if (this.Logger != null)
+                    this.Logger.LogVerbose("ApplyThisDecorationTo completed", null);
+            }
+
+            return rv;
+        }
+        #endregion
+    }
+
+
+}
