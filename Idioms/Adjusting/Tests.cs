5904d319f5160646a529144f30d35b1c5a87dfff
Piirtaa <piirtaa@gmail.com>
2014-11-04 21:44:25 -0500

    tests - adjustment

--- Idioms/Adjusting/Tests.cs
+++ Idioms/Adjusting/Tests.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Decoratid.Core.Contextual;
+using CuttingEdge.Conditions;
 
 namespace Decoratid.Idioms.Adjusting
 {
@@ -17,24 +18,55 @@ namespace Decoratid.Idioms.Adjusting
         public ConditionTest()
             : base(LogicOf<ICondition>.New((x) =>
             {
-                //the adjustment is an AND with a conditional bool
-                var condOf = StrategizedConditionOf<bool>.New((x) => { return x; });
-                //AddContext(true);
+                //tests "adjustment" idiom for Conditions
+                //we adjust by ANDing a switch that we turn off and on
 
-            })) 
-        { 
+                //get the old eval.  we'll be expecting this value with the switch on
+                var oldEval = x.Evaluate();
+
+                //the adjustment is an AND with a conditional bool that we switch on and off
+                var switchCond = StrategizedConditionOf<bool>.New((cond) => 
+                { 
+                    return new bool?(cond); 
+                }).AddContext(false);
+
+                //apply the adjustment
+                var newX = x.Adjust((cond) =>
+                {
+                    return cond.And(switchCond);
+                });
+
+                //if we evaluate the adjusted condition it should eval to false due to the switch condition
+                var adjustedEval = newX.Evaluate();
+                Condition.Requires(adjustedEval).IsEqualTo(false);
+
+                //switch on
+                switchCond.Context = true;
+                adjustedEval = newX.Evaluate();
+                Condition.Requires(adjustedEval).IsEqualTo(oldEval);
+            }))
+        {
         }
     }
 
-    public class ValueOfTest<T> : TestOf<IValueOf<T>>
+    public class ValueOfTest : TestOf<IValueOf<string>>
     {
         public ValueOfTest()
-            : base(LogicOf<IValueOf<T>>.New((x) =>
+            : base(LogicOf<IValueOf<string>>.New((x) =>
             {
-                //TESTS HERE
+                //tests "adjustment" idiom for ValueOf
+                //the adjustment appends a value "..Bro..." to the string
+                string appendText = "..Bro...";
 
+                var oldVal = x.GetValue();
 
+                var adjusted = x.Adjust((old) =>
+                {
+                    return old + appendText;
+                });
 
+                var newVal = adjusted.GetValue();
+                Condition.Requires(newVal).EndsWith(appendText);
 
             }))
         {
@@ -46,11 +78,22 @@ namespace Decoratid.Idioms.Adjusting
         public LogicTest()
             : base(LogicOf<ILogic>.New((x) =>
             {
-                //TESTS HERE
+                //tests "adjustment" idiom for logic
+                //the adjustment changes the value of a variable
+                var switchVar = false;
 
+                var adjusted = x.Adjust((old) =>
+                {
+                    var adjustedLogic = Logic.New(() =>
+                    {
+                        old.Perform();
+                        switchVar = true;
+                    });
+                    return adjustedLogic;
+                });
 
-
-
+                adjusted.Perform();
+                Condition.Requires(switchVar).IsTrue();
             }))
         {
         }
