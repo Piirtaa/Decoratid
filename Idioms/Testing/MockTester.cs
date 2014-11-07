a7615d5f877f68cf67642a4234e740cc7eabc2cf
Piirtaa <piirtaa@gmail.com>
2014-11-07 17:22:38 -0500

    mock tester

--- Idioms/Testing/MockTester.cs
+++ Idioms/Testing/MockTester.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Decoratid.Extensions;
+using Decoratid.Core.Logical;
+using Decoratid.Core.Conditional;
+using Decoratid.Core.ValueOfing;
+using Decoratid.Core.Identifying;
 
 namespace Decoratid.Idioms.Testing
 {
@@ -13,7 +17,16 @@ namespace Decoratid.Idioms.Testing
         {
 
             var results1 = TestOfTester.AutomaticTest<Nothing>(Nothing.VOID);
-            //results1.WithEach(
+            TestOfTester.LogTestResults(results1, "VoidTests.txt");
+
+            var results2 = TestOfTester.AutomaticTest<ILogic>(MockBuilder.BuildMockLogic());
+            TestOfTester.LogTestResults(results2, "LogicTests.txt");
+
+            var results3 = TestOfTester.AutomaticTest<ICondition>(MockBuilder.BuildMockCondition());
+            TestOfTester.LogTestResults(results3, "ConditionTests.txt");
+
+            var results4 = TestOfTester.AutomaticTest<IValueOf<IHasId>>(MockBuilder.BuildMockValueOf("id"));
+            TestOfTester.LogTestResults(results4, "ValueOfTests.txt");
         }
     }
 }
