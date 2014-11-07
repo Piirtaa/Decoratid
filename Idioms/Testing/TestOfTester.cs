b57aba46db5bf932873d466211e52041cb3b599d
Piirtaa <piirtaa@gmail.com>
2014-11-07 13:47:20 -0500

    testing framework stuff

--- Idioms/Testing/TestOfTester.cs
+++ Idioms/Testing/TestOfTester.cs
@@ -1,16 +1,102 @@
-﻿using System;
+﻿using Decoratid.Idioms.TypeLocating;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Decoratid.Extensions;
 
 namespace Decoratid.Idioms.Testing
 {
     public static class TestOfTester
     {
-        public static void Test<T>(T item)
+        /// <summary>
+        /// using type location, examines the appdomain for ITestOfs
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static List<ITestOf<T>> LocateTests<T>()
         {
-            //ITestOf<T>
+            Type genType = typeof(ITestOf<T>);
+            var tests = TheTypeLocator.Instance.Locator.Locate((t) =>
+            {
+                return t.HasGenericDefinition(genType);
+            });
+
+            List<ITestOf<T>> rv = new List<ITestOf<T>>();
+
+            tests.WithEach((testType) =>
+            {
+
+                ITestOf<T> test = (ITestOf<T>)Activator.CreateInstance(testType);
+                rv.Add(test);
+            });
+
+            return rv;
         }
+        /// <summary>
+        /// Performs a single test
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="item"></param>
+        /// <param name="test"></param>
+        /// <returns></returns>
+        public static TestOfResult Test<T>(T item, ITestOf<T> test)
+        {
+            try
+            {
+                test.Test(item);
+                return new TestOfResult() { IsTestSuccess = true, TestType = test.GetType() };
+            }
+            catch (Exception ex)
+            {
+                return new TestOfResult() { TestError=ex, TestType = test.GetType() };
+            }
+        }
+
+        public static List<TestOfResult> Test<T>(T item, List<ITestOf<T>> tests)
+        {
+            List<TestOfResult> rv = new List<TestOfResult>();
+            tests.WithEach((test) =>
+            {
+                rv.Add(Test(item, test));
+
+            });
+            return rv;
+        }
+        public static List<TestOfResult> AutomaticTest<T>(T item)
+        {
+            List<TestOfResult> rv = new List<TestOfResult>();
+            var tests = LocateTests<T>();
+            tests.WithEach((test) =>
+            {
+                rv.Add(Test(item, test));
+            });
+            return rv;
+        }
+        public static bool CheckTestResults(List<TestOfResult> results)
+        {
+            if (results == null)
+                return false;
+            if (results.Count == 0)
+                return false;
+
+            foreach (var each in results)
+            {
+                if (each.TestError != null)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    public class TestOfResult
+    {
+        public Type TestType { get; set; }
+        public Exception TestError { get; set; }
+        public bool IsTestSuccess { get; set; }
     }
 }
