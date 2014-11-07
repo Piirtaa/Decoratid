df7b26b7b9711545b26935608fddd9be2c9c2e90
Piirtaa <piirtaa@gmail.com>
2014-11-07 12:15:43 -0500

    validating test

--- Idioms/Validating/Tests.cs
+++ Idioms/Validating/Tests.cs
@@ -1,4 +1,6 @@
-﻿using Decoratid.Core.Conditional;
+﻿using CuttingEdge.Conditions;
+using Decoratid.Core.Conditional;
+using Decoratid.Core.Conditional.Of;
 using Decoratid.Core.Logical;
 using Decoratid.Core.ValueOfing;
 using Decoratid.Idioms.Testing;
@@ -7,6 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Decoratid.Core.Contextual;
 
 namespace Decoratid.Idioms.Validating
 {
@@ -15,11 +18,23 @@ namespace Decoratid.Idioms.Validating
         public ConditionTest()
             : base(LogicOf<ICondition>.New((x) =>
             {
-                //TESTS HERE
+                var cond = StrategizedConditionOf<bool>.New((o) => { return o; }).AddContext(false);
+                var valid = x.KackUnless(cond);
 
+                bool isValid = false;
+                try
+                {
+                    valid.Evaluate();
+                }
+                catch
+                {
+                    isValid = true;
+                }
+                Condition.Requires(isValid).IsTrue();
 
-
-
+                cond.Context = true;
+                valid.Evaluate();
+   
             })) 
         { 
         }
@@ -30,10 +45,22 @@ namespace Decoratid.Idioms.Validating
         public ValueOfTest()
             : base(LogicOf<IValueOf<T>>.New((x) =>
             {
-                //TESTS HERE
+                var cond = StrategizedConditionOf<bool>.New((o) => { return o; }).AddContext(false);
+                var valid = x.KackUnless(cond);
 
+                bool isValid = false;
+                try
+                {
+                    valid.GetValue();
+                }
+                catch
+                {
+                    isValid = true;
+                }
+                Condition.Requires(isValid).IsTrue();
 
-
+                cond.Context = true;
+                valid.GetValue();
 
             }))
         {
@@ -45,10 +72,22 @@ namespace Decoratid.Idioms.Validating
         public LogicTest()
             : base(LogicOf<ILogic>.New((x) =>
             {
-                //TESTS HERE
+                var cond = StrategizedConditionOf<bool>.New((o) => { return o; }).AddContext(false);
+                var valid = x.KackUnless(cond);
 
+                bool isValid = false;
+                try
+                {
+                    valid.Perform();
+                }
+                catch
+                {
+                    isValid = true;
+                }
+                Condition.Requires(isValid).IsTrue();
 
-
+                cond.Context = true;
+                valid.Perform();
 
             }))
         {
