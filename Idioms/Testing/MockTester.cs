b920ae7e7f6ddcae2abfe8c69e6f0a36df1d02cc
Piirtaa <piirtaa@gmail.com>
2014-11-24 01:40:55 -0500

    ref build

--- Idioms/Testing/MockTester.cs
+++ Idioms/Testing/MockTester.cs
@@ -21,46 +21,46 @@ namespace Decoratid.Idioms.Testing
         public static void AutoTestMocks()
         {
 
-            //var stringableListResults = TestOfTester.AutomaticTest<IStringableList>(NaturalStringableList.New("one", "two", "three"));
-            //TestOfTester.LogTestResults(stringableListResults, "StringableListTests.txt");
-            //Condition.Requires(TestOfTester.CheckTestResults(stringableListResults)).IsTrue();
+            var stringableListResults = TestOfTester.AutomaticTest<IStringableList>(NaturalStringableList.New("one", "two", "three"));
+            TestOfTester.LogTestResults(stringableListResults, "StringableListTests.txt");
+            Condition.Requires(TestOfTester.CheckTestResults(stringableListResults)).IsTrue();
 
-            //var stringableResults = TestOfTester.AutomaticTest<IStringable>(NaturalStringable.New("test"));
-            //TestOfTester.LogTestResults(stringableResults, "StringableTests.txt");
-            //Condition.Requires(TestOfTester.CheckTestResults(stringableResults)).IsTrue();
+            var stringableResults = TestOfTester.AutomaticTest<IStringable>(NaturalStringable.New("test"));
+            TestOfTester.LogTestResults(stringableResults, "StringableTests.txt");
+            Condition.Requires(TestOfTester.CheckTestResults(stringableResults)).IsTrue();
 
-            //var results1 = TestOfTester.AutomaticTest<Nothing>(Nothing.VOID);
-            //TestOfTester.LogTestResults(results1, "VoidTests.txt");
-            //Condition.Requires(TestOfTester.CheckTestResults(results1)).IsTrue();
+            var results1 = TestOfTester.AutomaticTest<Nothing>(Nothing.VOID);
+            TestOfTester.LogTestResults(results1, "VoidTests.txt");
+            Condition.Requires(TestOfTester.CheckTestResults(results1)).IsTrue();
 
-            //var results2 = TestOfTester.AutomaticTest<ILogic>(MockBuilder.BuildMockLogic());
-            //TestOfTester.LogTestResults(results2, "LogicTests.txt");
-            //Condition.Requires(TestOfTester.CheckTestResults(results2)).IsTrue();
+            var results2 = TestOfTester.AutomaticTest<ILogic>(MockBuilder.BuildMockLogic());
+            TestOfTester.LogTestResults(results2, "LogicTests.txt");
+            Condition.Requires(TestOfTester.CheckTestResults(results2)).IsTrue();
 
-            //var results3 = TestOfTester.AutomaticTest<ICondition>(MockBuilder.BuildMockCondition());
-            //TestOfTester.LogTestResults(results3, "ConditionTests.txt");
-            //Condition.Requires(TestOfTester.CheckTestResults(results3)).IsTrue();
+            var results3 = TestOfTester.AutomaticTest<ICondition>(MockBuilder.BuildMockCondition());
+            TestOfTester.LogTestResults(results3, "ConditionTests.txt");
+            Condition.Requires(TestOfTester.CheckTestResults(results3)).IsTrue();
 
-            //var results4 = TestOfTester.AutomaticTest<IValueOf<IHasId>>(MockBuilder.BuildMockValueOf("id"));
-            //TestOfTester.LogTestResults(results4, "ValueOfTests.txt");
-            //Condition.Requires(TestOfTester.CheckTestResults(results4)).IsTrue();
+            var results4 = TestOfTester.AutomaticTest<IValueOf<IHasId>>(MockBuilder.BuildMockValueOf("id"));
+            TestOfTester.LogTestResults(results4, "ValueOfTests.txt");
+            Condition.Requires(TestOfTester.CheckTestResults(results4)).IsTrue();
 
-            //var results5 = TestOfTester.AutomaticTest<IValueOf<string>>("derp".AsNaturalValue());
-            //TestOfTester.LogTestResults(results5, "ValueOfStringTests.txt");
-            //Condition.Requires(TestOfTester.CheckTestResults(results5)).IsTrue();
+            var results5 = TestOfTester.AutomaticTest<IValueOf<string>>("derp".AsNaturalValue());
+            TestOfTester.LogTestResults(results5, "ValueOfStringTests.txt");
+            Condition.Requires(TestOfTester.CheckTestResults(results5)).IsTrue();
 
-            //var results6 = TestOfTester.AutomaticTest<Host>(Host.New(EndPoint.NewFreeLocalEndPoint()));
-            //TestOfTester.LogTestResults(results6, "HostTests.txt");
-            //Condition.Requires(TestOfTester.CheckTestResults(results6)).IsTrue();
+            var results6 = TestOfTester.AutomaticTest<Host>(Host.New(EndPoint.NewFreeLocalEndPoint()));
+            TestOfTester.LogTestResults(results6, "HostTests.txt");
+            Condition.Requires(TestOfTester.CheckTestResults(results6)).IsTrue();
 
             var results7 = TestOfTester.AutomaticTest<IStore>(MockBuilder.BuildMockStore());
             TestOfTester.LogTestResults(results7, "StoreTests.txt");
             bool good = TestOfTester.CheckTestResults(results7);
             Condition.Requires(TestOfTester.CheckTestResults(results7)).IsTrue();
 
-            //var results8 = TestOfTester.AutomaticTest<Tuple<IStore, IHasId>>(new Tuple<IStore, IHasId>(MockBuilder.BuildMockStore(), MockBuilder.BuildMockIHasId("1")));
-            //TestOfTester.LogTestResults(results8, "StoreItemTests.txt");
-            //Condition.Requires(TestOfTester.CheckTestResults(results8)).IsTrue();
+            var results8 = TestOfTester.AutomaticTest<Tuple<IStore, IHasId>>(new Tuple<IStore, IHasId>(MockBuilder.BuildMockStore(), MockBuilder.BuildMockIHasId("1")));
+            TestOfTester.LogTestResults(results8, "StoreItemTests.txt");
+            Condition.Requires(TestOfTester.CheckTestResults(results8)).IsTrue();
         }
     }
 }
